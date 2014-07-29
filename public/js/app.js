67a3daad495d6c3ff55caf34a11cabdc41d8e82a
daniel1984 <daniel.stenger@yahoo.com>
2014-07-30 00:32:58 +0300

    new idea fpr skills page

--- public/js/app.js
+++ public/js/app.js
@@ -958,9 +958,21 @@
 
   module.exports = function(ctrl) {
     var view = m("div.skills-p.row-fluid.fade-out", { config: ctrl.isReady }, [
-      ctrl.imagesList.map(function(img, i) {
-        return m("img.physics [src='/img/logo/" + img + ".png']");
-      })
+//      ctrl.imagesList.map(function(img, i) {
+//        return m("img.physics [src='/img/logo/" + img + ".png']");
+//      })
+      m('div.col-sm-6.col-lg-6.col-xs-12', [
+        m('div.progress', [
+          m('div.progress-bar.progress-bar-success', { style : { width: '90%' }}, [
+            m('span', 'Javascript 90%')
+          ])
+        ]),
+        m('div.progress', [
+          m('div.progress-bar.progress-bar-success', { style : { width: '70%' }}, [
+            m('span', 'NodeJS 70%')
+          ])
+        ])
+      ])
     ]);
 
     return view;
