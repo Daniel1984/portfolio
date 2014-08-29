108559e5bb9938816ae71fb1f243b99e67da14db
daniel1984 <daniel.stenger@yahoo.com>
2014-08-29 22:33:53 +0300

    markup cleanup

--- app/assets/js/views/tiles.js
+++ app/assets/js/views/tiles.js
@@ -12,40 +12,35 @@
         m('div.body', [
           m('h2', 'Daniel Stenger'),
           m('h2', 'Javascript developer'),
-        ]),
-        m('div.inner-item')
+        ])
       ]),
 
       m("div.tile-item.skills [data-module='skills']", { style: { background: '#dda0dd' }, onclick: ctrl.onItemClick.bind(ctrl) }, [
         m('div.body', [
           m('i.fa.fa-magic'),
           m('h2', 'Skills')
-        ]),
-        m('div.inner-item')
+        ])
       ]),
 
       m("div.tile-item.portfolio [data-module='portfolio']", { style: { background: '#fada72' }, onclick: ctrl.onItemClick.bind(ctrl) }, [
         m('div.body', [
           m('i.fa.fa-suitcase'),
           m('h2', 'Portfolio')
-        ]),
-        m('div.inner-item')
+        ])
       ]),
 
       m("div.tile-item.experience [data-module='experience']", { style: { background: '#ff8a80' }, onclick: ctrl.onItemClick.bind(ctrl) }, [
         m('div.body', [
           m('i.fa.fa-clock-o'),
           m('h2', 'Experience')
-        ]),
-        m('div.inner-item')
+        ])
       ]),
 
       m("div.tile-item.contact [data-module='contact']", { style: { background: '#b388ff' }, onclick: ctrl.onItemClick.bind(ctrl) }, [
         m('div.body', [
           m('i.fa.fa-envelope'),
           m('h2', 'Contact')
-        ]),
-        m('div.inner-item')
+        ])
       ])
 
     ]);
