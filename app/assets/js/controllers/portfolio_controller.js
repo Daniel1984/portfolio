04e91b878bd0473961ce7df2b4951583fc36ae6d
daniel1984 <daniel.stenger@yahoo.com>
2014-10-25 02:49:03 +0300

    another portfolio item

--- app/assets/js/controllers/portfolio_controller.js
+++ app/assets/js/controllers/portfolio_controller.js
@@ -6,13 +6,14 @@
     pt: require('../modules/portfolio/pt'),
     ss: require('../modules/portfolio/ss'),
     plane: require('../modules/portfolio/plane'),
+    danny_jump: require('../modules/portfolio/danny_jump'),
     slot: require('../modules/portfolio/slot'),
     orbit: require('../modules/portfolio/orbit'),
     casino: require('../modules/portfolio/casino')
   };
 
   module.exports = function() {
-    this.portfolio_items = ['ssc','pt','ss','plane','slot','orbit','casino'];
+    this.portfolio_items = ['ssc','pt','ss','plane','danny_jump','slot','orbit','casino'];
   
     this.hideView = function() {
       document.querySelector('.animate').classList.remove('animate');
--- app/assets/js/views/portfolio/danny_jump.js
+++ app/assets/js/views/portfolio/danny_jump.js
@@ -1,27 +1,25 @@
 (function() {
   'use strict';
 
-  var m = require('mithril');
-
   module.exports = function(ctrl) {
     return m('div.ssc.portfolio-item-inner.fade-out', { config: ctrl.isReady }, [
       m('div.back-top-btn', { onclick: ctrl.fadeOutView }, [
         m('i.fa.fa-long-arrow-left')
       ]),
       m('div.header', [
-        m('h1', 'Flappy plane'),
+        m('h1', 'Danny Jump'),
         m('hr.star-light')
       ]),
       m('div.col-xs-12.col-sm-8.col-md-8.col-lg-8.col-sm-offset-2.col-md-offset-2.col-lg-offset-2', [
-        m("img[src='/img/portfolio/plane.png']"),
+        m("img[src='/img/portfolio/danny_jump.png']"),
         m('div.row.content', [
           m('div.col-xs-12.col-sm-12.col-md-6.col-lg-6', [
             m('blockquote', [ m('h4', 'About') ]),
-            m('p', 'My first game build with canvas. Automated development process with gulp and browserify.')
+            m('p', 'Yet another result of my habby. HTML5 game build with pixi.js. It\'s not 100% finished, so don\' judge too fast ;)')
           ]),
           m('div.col-xs-12.col-sm-12.col-md-6.col-lg-6', [
             m('blockquote', [ m('h4', 'Address') ]),
-            m("a[href='https://min60fps.herokuapp.com/fp/'][target='_blank']", 'http://www.flappyplane.com')
+            m("a[href='https://min60fps.herokuapp.com/jj/'][target='_blank']", 'http://www.flappyplane.com')
           ]),
           m('div.col-xs-12.col-sm-12.col-md-12.col-lg-12', [
             m('blockquote', [ m('h4', 'Tools used') ]),
